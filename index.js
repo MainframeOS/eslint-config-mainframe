5e11a4caaa8f013f2fc18de6177619300dbaba07
Paul Le Cam <paul@mainframe.com>
2018-05-24 12:47:57 +0100

    Add import plugin

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:flowtype/recommended', 'prettier', 'prettier/flowtype'],
+  extends: [
+    'plugin:flowtype/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+    'prettier/flowtype',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['flowtype', 'prettier'],
+  plugins: ['flowtype', 'import', 'prettier'],
   rules: {
     'prettier/prettier': ['error', require('./prettier.config')],
   },
